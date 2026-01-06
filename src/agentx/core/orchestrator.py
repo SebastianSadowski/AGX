9ed0cf8c13b4b80e8ce04776ad247806f55c9992
sebastian <sebastian.sadowski@tietoevry.com>
2026-01-06 23:19:14 +0100

    STG_3 - planner, simple planner logic, creates plan for given message & context. Orchestrator delegates responsibility to Planning interface and consume result

--- src/agentx/core/orchestrator.py
+++ src/agentx/core/orchestrator.py
@@ -12,17 +12,20 @@ Output: AgentResponse
 
 """
 from agentx.api.models import AgentResponse
+from agentx.core.types import PlanStep
 from agentx.memory.context_builder import *
 from typing import Optional
 
 from agentx.memory.session_store import MessageRole
+from agentx.planning.base import Planning, STEP_TYPE_GENERATE_RESPONSE, STEP_TYPE_CALL_TOOL
+from agentx.planning.simple_planner import SimplePlanner
 
 
 class Orchestrator:
-    def __init__(self, context_builder: Optional[ContextBuilder] = None, session_store: Optional[InMemorySessionStore] = None ):
-        print('hello')
+    def __init__(self, context_builder: Optional[ContextBuilder] = None, session_store: Optional[InMemorySessionStore] = None, planner: Optional[Planning] = None):
         self.session_store= session_store or InMemorySessionStore()
         self.context_builder = context_builder or ContextBuilder(self.session_store)
+        self.planner: Planning = planner or SimplePlanner()
 
 
     def run(self, request: AgentRequest) -> AgentResponse:
@@ -44,18 +47,31 @@ class Orchestrator:
         self._update_session(user_id, message, 'user')
         # here - composes response message
         history_len = len(history)
+
+        plan = self.planner.make_plan(request=request, context=context)
+
+        plan_response = self._handle_step(plan.steps[0])
+
+
         final_text = (
             f"[orchestrator] Kanał={channel}, user={user_id}, "
             f"wiadomości w historii: {history_len}. "
             f"Ostatnia wiadomość: {message}"
+            f"\n odpowiedz z planu {plan_response}"
         )
 
 
         # here - composes trace log
         trace = {
+            "plan": {
+                "steps_amount": len(plan.steps),
+                "steps": [ st.description for st in plan.steps]
+
+            },
             "steps": [
                 "orchestrator_start",
                 "single_step_reply",
+                [f"{st.description} ({st.step_type})" for st in plan.steps],
                 "orchestrator_end"
             ],
             "context_summary":{
@@ -83,3 +99,17 @@ class Orchestrator:
 
     def _update_session(self, user_id: str, message: str, role=MessageRole) -> None:
         self.session_store.append_message(user_id=user_id, message=message, role=role)
+
+    def _handle_step(self, step: PlanStep):
+        step_response: str = ""
+        if step.step_type == STEP_TYPE_GENERATE_RESPONSE:
+            print("Orchestrator._handle_step: step type GENERATE_RESPONSE…")
+            step_response = "Typ kroku GENERATE_RESPONSE…"
+        elif step.step_type== STEP_TYPE_CALL_TOOL:
+            print("Orchestrator._handle_step: step type CALL_TOOL…")
+            step_response = "Typ kroku CALL_TOOL"
+        else:
+            print("Orchestrator._handle_step: step type NOT_RECOGNIZED…")
+            step_response = "Nie rozpoznano typu kroku, zwracam blad"
+
+        return step_response
--- src/agentx/core/types.py
+++ src/agentx/core/types.py
@@ -1,4 +1,17 @@
-from typing import Any, Dict
-
+from typing import Any, Dict, Literal, List, Optional
+from pydantic import BaseModel
 
 Context = Dict[str, Any]
+StepType = Literal["GENERATE_RESPONSE", "CALL_TOOL"]
+
+
+class PlanStep(BaseModel):
+    step_type: StepType
+    description: str #short explanation e.g. explain to user, call math tool
+    tool_name: Optional[str] = None
+    params: Optional[dict] = None
+
+class Plan(BaseModel):
+    steps: List[PlanStep]
+
+
